e24f4cf357130507bb7ac294b6d676dc0ce90ca7
juliusyam <juliusyam@users.noreply.github.com>
2021-06-29 13:14:18 +0100

    inital viewport state management with item slice in map

--- src/components/RelatedProducts.tsx
+++ src/components/RelatedProducts.tsx
@@ -1,15 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { carouselData } from '../data/data.json'; // use useEffect for retrieving data from a REST API
 import { CarouselData } from '../models/APIModels';
 import useCurrency from 'currency-symbol-map';
-import { Button } from '@chakra-ui/react';
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import { useViewport, Width } from "../services/useViewport";
+import { Button, IconButton } from '@chakra-ui/react';
+import { ViewIcon, ViewOffIcon, ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import { Color } from '../utilities/Color';
 
 export function RelatedProducts() {
 
   const [showMore, setShowMore] = useState<boolean>(false);
+  const { width } = useViewport();
+  const [productDisplayQuantity, setProductDisplayQuantity] = useState<number>();
+  const pageAmount = productDisplayQuantity && Math.ceil(carouselData.length / productDisplayQuantity);
+  const [pageCount, setPageCount] = useState(1);
+  const [firstItem, setFirstItem] = useState(0);
+  const [lastItem, setLastItem] =  useState<number>();
 
+  useEffect(() => {
+    setFirstItem(0);
+
+    if (width < Width.Phone) {
+      setProductDisplayQuantity(1);
+      setLastItem(1);
+    } else if (width < Width.SmallTablet) {
+      setProductDisplayQuantity(2);
+      setLastItem(2);
+    } else if (width < Width.LargeTablet) {
+      setProductDisplayQuantity(3);
+      setLastItem(3);
+    } else {
+      setProductDisplayQuantity(4);
+      setLastItem(4);
+    }
+  }, [width]);
 
   return (
     <section className="related-products">
@@ -25,14 +49,31 @@ export function RelatedProducts() {
         </Button>
       </div>
       { showMore ?
-        <section className="products-list">
+        <section className="products-list" style={ { gridTemplateColumns: `repeat(${ productDisplayQuantity }, 1fr`} }>
           { carouselData
             //Remove display if !data.showCarouselItemHead
             .filter(data => data.showCarouselItemHead)
+            .slice(firstItem, lastItem)
             //Sort based on data.itemPositionOneBased in chronological order
             .sort((a, b) => a.itemPositionOneBased - b.itemPositionOneBased)
             .map(data =>
               <Product data={ data } showMore={ showMore } key={ data.code } />) }
+          <IconButton className="control left" aria-label="left-icon" icon={ <ChevronLeftIcon w={7} h={7} /> }
+            onClick={ () => {
+              if (productDisplayQuantity && lastItem) {
+                setFirstItem(firstItem - productDisplayQuantity);
+                setLastItem(lastItem - productDisplayQuantity);
+                setPageCount(pageCount - 1);
+              }
+            } } />
+          <IconButton className="control right" aria-label="right-icon" icon={ <ChevronRightIcon w={7} h={7} /> }
+            onClick={ () => {
+              if (productDisplayQuantity && lastItem) {
+                setFirstItem(firstItem + productDisplayQuantity);
+                setLastItem(lastItem + productDisplayQuantity);
+                setPageCount(pageCount + 1);
+              }
+            } } />
         </section> :
         <section className="products-list collapsed-product-list">
           { carouselData
---
+++ src/services/useViewport.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+
+export function useViewport() {
+
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    }
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => window.removeEventListener('resize', handleWindowResize);
+  }, []);
+
+  // const { width, height } = useViewport(); to retrieve width and height of screen
+  return { width, height };
+}
+
+export enum Width {
+  Phone = 600,
+  SmallTablet = 992,
+  LargeTablet = 1200,
+}
+
+export enum DashboardDisplayHeight {
+  SmallTablet = '100vh - 4em',
+  Desktop = '100vh',
+}
