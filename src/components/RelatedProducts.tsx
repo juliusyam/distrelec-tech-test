021e6be73fbcd1278c48dc36567722938925b8b7
juliusyam <juliusyam@users.noreply.github.com>
2021-09-21 17:59:24 +0100

    improve the state management of page count

--- src/components/RelatedProducts.tsx
+++ src/components/RelatedProducts.tsx
@@ -7,35 +7,44 @@ import { Button, IconButton } from '@chakra-ui/react';
 import { ViewIcon, ViewOffIcon, ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import { Color } from '../utilities/Color';
 
+interface ItemDisplayState {
+  displayQuantity: number,
+  pageCount: number,
+  firstItem: number,
+  lastItem: number,
+}
 export function RelatedProducts() {
 
   const [showMore, setShowMore] = useState<boolean>(false);
   const { width } = useViewport();
-  const [productDisplayQuantity, setProductDisplayQuantity] = useState<number>();
-  const pageAmount = productDisplayQuantity && Math.ceil(carouselData.length / productDisplayQuantity);
-  const [pageCount, setPageCount] = useState(1);
-  const [firstItem, setFirstItem] = useState(0);
-  const [lastItem, setLastItem] =  useState<number>();
 
-  useEffect(() => {
-    //If width changes, reset the firstItem in display to be 0, then check lastItem based on width.
-    setFirstItem(0);
-    setPageCount(1);
+  const [resultDisplay, setResultDisplay] = useState<ItemDisplayState>({
+    displayQuantity: 0,
+    firstItem: 0,
+    lastItem: 0,
+    pageCount: 1,
+  });
+  const { firstItem, lastItem, displayQuantity, pageCount } = resultDisplay;
 
-    if (width < Width.Phone) {
-      setProductDisplayQuantity(1);
-      setLastItem(1);
-    } else if (width < Width.SmallTablet) {
-      setProductDisplayQuantity(2);
-      setLastItem(2);
-    } else if (width < Width.LargeTablet) {
-      setProductDisplayQuantity(3);
-      setLastItem(3);
-    } else {
-      setProductDisplayQuantity(4);
-      setLastItem(4);
+  const resetSelectionState = () => {
+    setResultDisplay(resultDisplay => ({ ...resultDisplay, firstItem: 0, pageCount: 1 }));
+
+    switch (true) {
+      case width <= Width.Phone:
+        setResultDisplay({ firstItem: 0, lastItem: 1,  pageCount: 1, displayQuantity: 1 });
+        break;
+      case width <= Width.SmallTablet:
+        setResultDisplay({ firstItem: 0, lastItem: 2,  pageCount: 1, displayQuantity: 2 });
+        break;
+      case width <= Width.LargeTablet:
+        setResultDisplay({ firstItem: 0, lastItem: 3,  pageCount: 1, displayQuantity: 3 });
+        break;
+      default:
+        setResultDisplay({ firstItem: 0, lastItem: 4,  pageCount: 1, displayQuantity: 4 });
     }
-  }, [width]);
+  };
+
+  useEffect(resetSelectionState, [width]);
 
   return (
     <section className="related-products">
@@ -51,7 +60,7 @@ export function RelatedProducts() {
         </Button>
       </div>
       { showMore ?
-        <section className="products-list" style={ { gridTemplateColumns: `repeat(${ productDisplayQuantity }, 1fr`} }>
+        <section className="products-list" style={ { gridTemplateColumns: `repeat(${ displayQuantity }, 1fr`} }>
           { carouselData
             //Remove display if !data.showCarouselItemHead
             .filter(data => data.showCarouselItemHead)
@@ -62,22 +71,20 @@ export function RelatedProducts() {
               <Product data={ data } showMore={ showMore } key={ data.code } />) }
           <IconButton className="control left" aria-label="left-icon" icon={ <ChevronLeftIcon w={7} h={7} /> }
                       disabled={ firstItem <= 0 }
-                      onClick={ () => {
-                        if (productDisplayQuantity && lastItem) {
-                          setFirstItem(firstItem - productDisplayQuantity);
-                          setLastItem(lastItem - productDisplayQuantity);
-                          setPageCount(pageCount - 1);
-                        }
-                      } } />
+                      onClick={ () => setResultDisplay({
+                        ...resultDisplay,
+                        firstItem: firstItem - displayQuantity,
+                        lastItem: lastItem - displayQuantity,
+                        pageCount: pageCount - 1
+                      }) } />
           <IconButton className="control right" aria-label="right-icon" icon={ <ChevronRightIcon w={7} h={7} /> }
-                      disabled={ pageAmount ? pageCount >= pageAmount : undefined }
-                      onClick={ () => {
-                        if (productDisplayQuantity && lastItem) {
-                          setFirstItem(firstItem + productDisplayQuantity);
-                          setLastItem(lastItem + productDisplayQuantity);
-                          setPageCount(pageCount + 1);
-                        }
-                      } } />
+                      disabled={ lastItem >= carouselData.length }
+                      onClick={ () => setResultDisplay({
+                        ...resultDisplay,
+                        firstItem: firstItem + displayQuantity,
+                        lastItem: lastItem + displayQuantity,
+                        pageCount: pageCount + 1
+                      }) } />
         </section> :
         <section className="products-list collapsed-product-list">
           { carouselData
